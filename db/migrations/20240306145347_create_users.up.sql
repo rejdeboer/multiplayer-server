2512f162e19a91febf7181b704fc82f0d3372940
rejdeboer <rick.deboer@live.nl>
2024-03-08 17:12:11 +0100

    fix: email should be unique

--- db/migrations/20240306145347_create_users.up.sql
+++ db/migrations/20240306145347_create_users.up.sql
@@ -2,8 +2,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
     id uuid PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
-	email text NOT NULL,
-	passhash text NOT NULL
+    email text NOT NULL UNIQUE,
+    passhash text NOT NULL
 );
 
 CREATE INDEX IF NOT EXISTS "idx_users_id" ON "users" ("id");
