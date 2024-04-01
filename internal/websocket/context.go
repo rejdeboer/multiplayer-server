b0533b31e4d4a20cbd8bcacb07e5b8e175aa575c
Rick de Boer <rick.deboer@live.nl>
2024-04-01 12:15:07 +0200

    feat: add logging to websocket context

--- internal/websocket/context.go
+++ internal/websocket/context.go
@@ -4,16 +4,25 @@ import (
 	"context"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/rejdeboer/multiplayer-server/internal/logger"
+	"github.com/rs/zerolog"
 )
 
 type Context struct {
+	Log        zerolog.Logger
 	UserID     string
 	BlobClient *azblob.Client
 }
 
 func CreateContext(ctx context.Context) Context {
+	userID := ctx.Value("user_id").(string)
+	log := logger.Get().With().
+		Str("user_id", userID).
+		Logger()
+
 	return Context{
-		UserID:     ctx.Value("user_id").(string),
+		Log:        log,
+		UserID:     userID,
 		BlobClient: ctx.Value("azblob").(*azblob.Client),
 	}
 }
