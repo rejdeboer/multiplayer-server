91ddccb90af8b94f97c67054a045d8b2cfc77a0d
rejdeboer <rick.deboer@live.nl>
2024-04-08 22:58:27 +0200

    feat: add create document route to router

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -22,6 +22,7 @@ func NewRouter(settings configuration.ApplicationSettings) http.Handler {
 	})
 
 	mux.HandleFunc("POST /user", createUser)
+	mux.HandleFunc("POST /document", createDocument)
 	mux.HandleFunc("POST /token", getToken(settings.SigningKey))
 	mux.HandleFunc("GET /websocket", middleware.WithAuth(handleWebSocket(hub), settings.SigningKey))
 
