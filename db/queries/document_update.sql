52b09bae7000d8facdb9ab760bde5caf33678b5a
rejdeboer <rick.deboer@live.nl>
2024-04-19 19:45:30 +0200

    chore: remove unused document_update queries

--- db/queries/document_update.sql
+++
@@ -1,8 +0,0 @@
--- name: GetDocumentClock :one
-SELECT COALESCE(MAX(clock), 0) 
-FROM document_updates
-WHERE document_id = $1;
-
--- name: CreateDocumentUpdate :exec
-INSERT INTO document_updates (document_id, clock, value)
-    VALUES ($1, $2, $3);
