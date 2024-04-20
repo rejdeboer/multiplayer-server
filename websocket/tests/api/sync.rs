7945cbdcf7be569f18fa7d8ddaba5d2a8dde3c36
rejdeboer <rick.deboer@live.nl>
2024-04-20 15:42:18 +0200

    feat: improve get_diff test

--- websocket/tests/api/sync.rs
+++ websocket/tests/api/sync.rs
@@ -1,28 +1,11 @@
 use futures::{SinkExt, StreamExt};
 use tokio_tungstenite::tungstenite;
+use yrs::{
+    updates::{decoder::Decode, encoder::Encode},
+    Doc, GetString, ReadTxn, StateVector, Text, Transact, Update,
+};
 
 use crate::helpers::spawn_app;
-
-/* Generated via:
-    ```js
-       const doc = new Y.Doc()
-       const ytext = doc.getText('type')
-       doc..transact_mut()(function () {
-           ytext.insert(0, 'def')
-           ytext.insert(0, 'abc')
-           ytext.insert(6, 'ghi')
-           ytext.delete(2, 5)
-       })
-       const update = Y.encodeStateAsUpdate(doc)
-       ytext.toString() // => 'abhi'
-    ```
-    a zero is appended for the sync message type
-*/
-const TEST_UPDATE: &[u8] = &[
-    1, 5, 152, 234, 173, 126, 0, 1, 1, 4, 116, 121, 112, 101, 3, 68, 152, 234, 173, 126, 0, 2, 97,
-    98, 193, 152, 234, 173, 126, 4, 152, 234, 173, 126, 0, 1, 129, 152, 234, 173, 126, 2, 1, 132,
-    152, 234, 173, 126, 6, 2, 104, 105, 1, 152, 234, 173, 126, 2, 0, 3, 5, 2, 0,
-];
 
 #[tokio::test]
 async fn other_client_receives_sync() {
@@ -47,25 +30,49 @@ async fn other_client_receives_sync() {
 #[tokio::test]
 async fn get_diff_after_update() {
     let app = spawn_app().await;
+    let server_sv = StateVector::default();
+
+    let doc_a = Doc::new();
+    let text_a = doc_a.get_or_insert_text("test");
+
+    {
+        let mut txn = doc_a.transact_mut();
+        text_a.push(&mut txn, "test");
+    }
+
+    let mut update = doc_a.transact().encode_diff_v1(&server_sv);
+    update.push(websocket::websocket::MESSAGE_SYNC);
+
     let mut client_a = app.create_owner_client().await;
-
-    let sync_payload = TEST_UPDATE.to_vec();
-    let get_diff_payload: Vec<u8> = vec![0, 1];
-
     client_a
-        .send(tungstenite::Message::Binary(sync_payload.clone()))
+        .send(tungstenite::Message::Binary(update.clone()))
         .await
         .unwrap();
 
-    client_a
-        .send(tungstenite::Message::Binary(get_diff_payload))
+    let doc_b = Doc::new();
+    let mut sv_b = doc_b.transact().state_vector().encode_v1();
+    sv_b.push(websocket::websocket::MESSAGE_GET_DIFF);
+
+    let mut client_b = app.create_owner_client().await;
+    client_b
+        .send(tungstenite::Message::Binary(sv_b))
         .await
         .unwrap();
 
-    let received = match client_a.next().await.unwrap().unwrap() {
+    let mut received = match client_b.next().await.unwrap().unwrap() {
         tungstenite::Message::Binary(payload) => payload,
         other => panic!("expected a binary message but got {other:?}"),
     };
 
-    assert_eq!(sync_payload, received);
+    let message_type = received.pop().unwrap();
+    assert_eq!(message_type, websocket::websocket::MESSAGE_SYNC);
+
+    doc_b
+        .transact_mut()
+        .apply_update(Update::decode_v1(&received).unwrap());
+    let text_b = doc_b.get_or_insert_text("test");
+    assert_eq!(
+        text_a.get_string(&doc_a.transact()),
+        text_b.get_string(&doc_b.transact())
+    );
 }
