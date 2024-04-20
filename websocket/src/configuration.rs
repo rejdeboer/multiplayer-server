353c4cf58b7c12ef5a3b35e5e445024589dd48a0
rejdeboer <rick.deboer@live.nl>
2024-04-20 19:59:53 +0200

    feat: override config using env variables

--- websocket/src/configuration.rs
+++ websocket/src/configuration.rs
@@ -96,5 +96,7 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         config::File::from(configuration_directory.join(environment.as_str())).required(true),
     )?;
 
+    settings.merge(config::Environment::with_prefix("app").separator("__"))?;
+
     settings.try_into()
 }
