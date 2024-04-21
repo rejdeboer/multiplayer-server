d4ebb3fd3b456d69cd83ea7bb76ec4d94d6d6a54
rejdeboer <rick.deboer@live.nl>
2024-04-21 20:57:16 +0200

    feat: add unique constraint to document updates

--- db/migrations/20240410151644_create_document_updates.up.sql
+++ db/migrations/20240410151644_create_document_updates.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE document_updates (
     document_id uuid NOT NULL,
     clock integer NOT NULL,
     value bytea NOT NULL,
-    PRIMARY KEY(document_id, clock)
+    PRIMARY KEY(document_id, clock),
+    UNIQUE(document_id, value)
 );
