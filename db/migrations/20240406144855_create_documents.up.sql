8ea458023799f5f74f76b2588f02539c9dd84ddc
rejdeboer <rick.deboer@live.nl>
2024-05-17 19:25:05 +0200

    feat: add owner_id foreign key directly to row

--- db/migrations/20240406144855_create_documents.up.sql
+++ db/migrations/20240406144855_create_documents.up.sql
@@ -1,12 +1,9 @@
 CREATE TABLE documents (
     id uuid PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
     name text NOT NULL,
-    owner_id uuid NOT NULL,
+    owner_id uuid NOT NULL REFERENCES users(id),
     shared_with uuid[] DEFAULT ARRAY[]::uuid[],
     state_vector bytea,
-    CONSTRAINT fk_user
-          FOREIGN KEY(owner_id) 
-            REFERENCES users(id)
 );
 
 CREATE INDEX IF NOT EXISTS "idx_documents_id" ON "documents" ("id");
