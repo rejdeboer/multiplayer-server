086af82184555ac0e6119516f36fba3b87317dd1
rejdeboer <rick.deboer@live.nl>
2024-05-19 00:48:05 +0200

    feat: user tries to add themselves as contributor test case

--- tests/api/contributor_test.go
+++ tests/api/contributor_test.go
@@ -14,15 +14,15 @@ func TestCreateContributor(t *testing.T) {
 	testApp := GetTestApp()
 	testDocID := testApp.document.ID
 
-	otherUser := createTestUser()
-	bodyBytes, err := json.Marshal(routes.DocumentContributorCreate{
-		UserID: otherUser.ID,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	t.Run("success response", func(t *testing.T) {
+		otherUser := createTestUser()
+		bodyBytes, err := json.Marshal(routes.DocumentContributorCreate{
+			UserID: otherUser.ID,
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+
 		req, err := http.NewRequest(
 			http.MethodPost,
 			"/document/"+testDocID.String()+"/contributor",
@@ -39,6 +39,43 @@ func TestCreateContributor(t *testing.T) {
 		status := rr.Result().StatusCode
 		if status != 202 {
 			t.Errorf("expected %d got %d", 202, rr.Result().StatusCode)
+		}
+	})
+
+	t.Run("user tries to add themselves", func(t *testing.T) {
+		otherUser := createTestUser()
+		bodyBytes, err := json.Marshal(routes.DocumentContributorCreate{
+			UserID: otherUser.ID,
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest(
+			http.MethodPost,
+			"/document/"+testDocID.String()+"/contributor",
+			bytes.NewReader(bodyBytes),
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+		token, err := routes.GetJwt(
+			settings.Application.SigningKey,
+			settings.Application.TokenExpirationSeconds,
+			otherUser.ID.String(),
+			otherUser.Username,
+		)
+		if err != nil {
+			t.Fatalf("error creating test token: %s", err)
+		}
+		req.Header.Add("Authorization", "Bearer "+token)
+
+		rr := httptest.NewRecorder()
+		testApp.handler.ServeHTTP(rr, req)
+
+		status := rr.Result().StatusCode
+		if status != 404 {
+			t.Errorf("expected %d got %d", 404, rr.Result().StatusCode)
 		}
 	})
 }
