2599eaa693999f8c334c36955f35004893f5944d
rejdeboer <rick.deboer@live.nl>
2024-05-18 10:36:29 +0200

    feat: add foreign key constraint to document updates

--- db/migrations/20240406144855_create_documents.up.sql
+++ db/migrations/20240406144855_create_documents.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS documents (
     id uuid PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
     name text NOT NULL,
-    owner_id uuid NOT NULL REFERENCES users(id),
+    owner_id uuid NOT NULL REFERENCES users(id)
+        ON DELETE CASCADE,
     state_vector bytea
 );
 
--- db/migrations/20240410151644_create_document_updates.up.sql
+++ db/migrations/20240410151644_create_document_updates.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS document_updates (
-    document_id uuid NOT NULL,
+    document_id uuid NOT NULL REFERENCES documents(id)
+        ON DELETE CASCADE,
     clock integer NOT NULL,
     value bytea NOT NULL,
     PRIMARY KEY(document_id, clock),
