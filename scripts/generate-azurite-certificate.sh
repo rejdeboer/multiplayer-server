ddb84b2ecc5ecda4b9ea30952a92077531555559
rejdeboer <rick.deboer@live.nl>
2024-06-20 11:07:36 +0200

    chore: remove unused certificate generation script

--- scripts/generate-azurite-certificate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-openssl req -x509 -newkey rsa:4096 -keyout az-dev-key.pem -out az-dev-cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=azurite" -addext "subjectAltName = DNS:azurite"
