30d0c4a3c5fdcf7f9eb5a0dfb693638eef862299
rejdeboer <rick.deboer@live.nl>
2024-06-27 18:58:16 +0200

    chore: clean up startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-docker-compose up -d elasticsearch kafka init-kafka kafka-connect websocket-api http-api postgres postgres-migration
+docker-compose up -d 
 
 # Wait till Elasticsearch is up
+echo "waiting for elasticsearch to start up"
 until curl -sS 'http://localhost:9200/_cat/health?h=status' | grep -q 'green\|yellow'; do
   sleep 1
 done
@@ -19,6 +20,7 @@ curl -X PUT 'http://localhost:9200/users' -H 'Content-Type: application/json' -d
 }'
 
 # blocks until kafka is reachable
+echo "waiting for kafka"
 kafka-topics --bootstrap-server localhost:9092 --list
 
 # Can be used to delete elasticsearch connector
@@ -31,14 +33,14 @@ curl -X POST http://localhost:8083/connectors -H 'Content-Type: application/json
     "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
     "tasks.max": "1",
     "topics": "users",
-	"key.ignore": "false",
+    "key.ignore": "false",
     "schema.ignore": "true",
     "connection.url": "http://elastic:9200",
     "type.name": "_doc",
     "name": "elasticsearch-sink",
     "value.converter": "org.apache.kafka.connect.json.JsonConverter",
     "value.converter.schemas.enable": "false",
-	"behaviour.on.null.vallues": "DELETE"
+    "behaviour.on.null.vallues": "DELETE"
   }
 }'
 
